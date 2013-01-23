fca2e43a485d4d7890a7137878526dc31275451f
Gary Larizza <gary@puppetlabs.com>
2013-01-23 10:12:47 -0800

    Scrolling up and down through search history
    This commit adds bindkeys so you can arrow up/down through your command
    search history.

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -8,3 +8,7 @@ export RUBYOPT=rubygems
 
 # Bind incremental history search
 bindkey "^r" history-incremental-search-backward
+
+# Use arrows to search back/forth in history search
+bindkey "\e[B" history-search-forward
+bindkey "\e[A" history-search-backward
