f907057e87c9832f3794aa258e355ae18d82c15f
Gary Larizza <gary@puppetlabs.com>
2013-11-21 15:23:12 -0500

    Fucking do it...

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -9,6 +9,9 @@ alias animal='ssh root@animal.dc1.puppetlabs.net'
 alias puppet='sudo envpuppet puppet'
 alias facter='sudo envpuppet facter'
 
+# Because
+alias fucking=sudo
+
 # Total disk usage for current folder
 alias use="/bin/ls -1A | tr '\n' '\0' | xargs -0 du -sk | sort -n"
 
